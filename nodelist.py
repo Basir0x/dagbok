09e82d3014cafd7daf82ea1cdbb8861c66059c6f
Basir0x <Basir.alizadeh@ha.ax>
2026-09-14 17:29:57 +0300

    Fix insert_sorted method in Monthlist class to handle session insertion correctly

--- nodelist.py
+++ nodelist.py
@@ -6,4 +6,15 @@ class Monthlist:
 
     def insert_sorted(self, session): 
         new_node = Node(session)
-        
+
+        if self.head is None or session.when < self.head.session.when:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next is not None and current.next.session.when < session.when:
+            current = current.next
+
+        new_node.next = current.next
+        current.next = new_node
